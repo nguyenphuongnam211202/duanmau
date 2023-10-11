0007310c4f6ea87280431106497e5fe494c21455
Nam <nguyenphuongnamnphn@example.com>
2023-10-11 22:15:19 +0700

    up

---
+++ view/footer.php
@@ -0,0 +1,49 @@
+<footer>
+            <div class="f1">
+                <h3>Thông tin liên hệ</h3>
+                <p>Địa chỉ:160 Nguyễn Huy Tưởng, Thanh Khê, Đà Nẵng</p>
+                <p>Số điện thoại: 0912832391</p>
+                <p>Email: Namnphpd09510@fpt.edu.vn</p>
+            </div>
+            <div class="f2">
+                <h3>Hỗ trợ khách hàng</h3>
+                <ul>
+                    <li><a href="">Tìm kiếm</a></li>
+                    <li><a href="">Giới thiệu</a></li>
+                    <li><a href="">Chính sách đổi trả</a></li>
+                    <li><a href="">Chính sách bảo mật</a></li>
+                    <li><a href="">Điều khoản dịch vụ</a></li>
+                </ul>
+            </div>
+            <div class="f3">
+                <h3>Theo dõi chúng tôi</h3>
+                <ul>
+                    <li><a href="">Facebook</a></li>
+                    <li><a href="">Tiktok</a></li>
+                    <li><a href="">Instagram</a></li>
+                </ul>
+                <h4>Follow mình nha! </h4>
+            </div>
+            <div class="f4">
+                <h3>Sản phẩm</h3>
+                <ul>
+                    <li><a href="">Điện thoại</a></li>
+                    <li><a href="">Laptop</a></li>
+                    <li><a href="">Ốp lưng</a></li>
+                    <li><a href="">Sạc dự phòng</a></li>
+                    <li><a href="">Bàn phím</a></li>
+                    <li><a href="">Tai nghe</a></li>
+                    <li><a href="">Đồng hồ</a></li>
+                    <li><a href="">Balo</a></li>
+                </ul>
+            </div>
+        </footer>
+        <h5 style="text-align:center ;"> Copyright 2023 © Nguyễn Phương Hoài Nam </h5>
+    </div>
+
+   
+
+
+</body>
+
+</html>
---
+++ view/lienhe.php
@@ -0,0 +1,129 @@
+
+<article>
+            <h1 style="text-align: center;" class="h1">Liên hệ trực tuyến</h1>
+            <div class="mxh">
+               <a href="#"><img src="./view/image/facebook.png" alt=""></a>
+               <a href="#"><img src="./view/image/instagram.png" alt=""></a>
+               <a href="#"><img src="./view/image/tiktok.png" alt=""></a>
+               <a href="#"><img src="./view/image/twitter.png" alt=""></a>
+            </div>
+
+            <h1 style="text-align: center;">Địa chỉ</h1>
+            <div class="ttlienhe">
+                Thông tin liên hệ <br>
+                Địa chỉ: 160 Nguyễn Huy Tưởng, Thanh Khê, Đà Nẵng <br>
+
+                Số điện thoại: 0912832391 <br>
+
+                Email:Namnphpd09510@fpt.edu.vn <br>
+            </div>
+            <h1 style="text-align: center; margin-bottom: 10px;">Bản đồ cửa hàng mình đây nè!!</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.3370121288744!2d108.1672676744825!3d16.047992140007395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421914e346fa69%3A0xbc8d642bf5033c38!2zMTYwIE5ndXnhu4VuIEh1eSBUxrDhu59uZywgSG_DoCBBbiwgQ-G6qW0gTOG7hywgxJDDoCBO4bq1bmcgNTUwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1696830667285!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+        </article>
+
+  
+        <aside>
+
+        <div class="phai">
+        <h1>Danh mục</h1>
+        <hr>
+        <ul>
+       
+
+            <li><a href="index.php">Trang chủ</a></li>
+            <hr>
+            <li><a href="index.php?act=sanpham">Cửa hàng</a></li>
+            <hr>
+            <li><a href="index.php?act=lienhe">Liên hệ</a></li>
+            <hr>
+            <li><a href="index.php?act=gioithieu">Giới thiệu</a></li>
+            <hr>
+            <li><a href="index.php?act=chinhsach">Chính sách đổi trả</a></li>
+            <hr>
+            <li><a href="index.php?act=chinhsach">Chính sách bảo mật</a></li>
+            <hr>
+            <li><a href="">Điều khoản dịch vụ</a></li>
+            <hr>
+        </ul>
+    </div>
+            <div class="slideshow">
+                <img src="./view/image/anh1.jpg" id="hinh" alt="">
+            </div>
+
+
+        </aside>
+        <script src="./view/js/slideshow.js"></script>
+        
+<style>
+    article {
+    height: 800px;
+    width: 70%;
+}
+
+.h1 {
+    margin-top: 20px;
+}
+
+.mxh {
+    margin-top: 10px;
+    text-align: center;
+    height: 50px;
+    line-height: 40px;
+    background-color: rgb(226, 226, 226);
+    margin-bottom: 20px;
+}
+
+.mxh img {
+    margin-top: 5px;
+    margin-left: 10px;
+    width: 40px;
+}
+
+.ttlienhe {
+    text-align: center;
+    background-color: rgb(226, 226, 226);
+    margin-top: 10px;
+    margin-bottom: 20px;
+
+}
+
+aside {
+    width: 30%;
+    float: right;
+    margin-top: -800px;
+    padding-left: 60px;
+    height: 600px;
+
+}
+
+.phai {
+
+    height: 400px;
+    padding-top: 10px;
+
+}
+
+.phai ul li {
+    display: block;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+
+.phai li a {
+    text-decoration: none;
+    color: black;
+
+}
+
+.phai li a:hover {
+    color: blueviolet;
+    text-decoration: underline;
+}
+
+.slideshow img {
+    width: 285px;
+}
+
+</style>
